39e2d8ba8bcbdf7affb65c536214c9091637b3e8
Pranav Konduru <konduru1976@mydusd.org>
2017-03-01 19:13:17 -0800

    added if-else

--- app/src/main/java/com/example/konduru91976/collegeapp/ApplicantActivity.java
+++ app/src/main/java/com/example/konduru91976/collegeapp/ApplicantActivity.java
@@ -93,6 +93,11 @@ public class ApplicantActivity extends AppCompatActivity
         } else if (id == R.id.nav_send) {
 
         }*/
+        if (id == R.id.family_member) {
+            //Hangdle the family member action
+        } else if (id == R.id.profile) {
+
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
