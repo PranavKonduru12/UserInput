6fe8fb58c53ee8efbce770cf4dc8f06fa6f6f94d
konduru91976 <konduru1976+gh@mydusd.org>
2017-03-06 11:43:44 -0800

    add files'

--- app/src/main/java/com/example/konduru91976/collegeapp/FamilyMemberFragment.java
+++ app/src/main/java/com/example/konduru91976/collegeapp/FamilyMemberFragment.java
@@ -15,6 +15,7 @@ import java.util.ArrayList;
 
 public class FamilyMemberFragment extends android.support.v4.app.Fragment {
 
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
