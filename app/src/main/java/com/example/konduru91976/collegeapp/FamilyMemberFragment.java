63cd2fc0abebfb517950b55db81e24003fd6c901
Pranav Konduru <konduru1976@mydusd.org>
2017-03-13 11:54:10 -0700

    Tried some things

--- app/src/main/java/com/example/konduru91976/collegeapp/FamilyMemberFragment.java
+++ app/src/main/java/com/example/konduru91976/collegeapp/FamilyMemberFragment.java
@@ -5,6 +5,8 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -14,12 +16,18 @@ import java.util.ArrayList;
  */
 
 public class FamilyMemberFragment extends android.support.v4.app.Fragment {
-    FamilyMemberFragment FamilyMember = new FamilyMemberFragment();
+    //FamilyMemberFragment FamilyMember = new FamilyMemberFragment();
+    private TextView mFirstName;
+    private EditText mEditFirstName;
+    private TextView mLastName;
+    private EditText mEditLastName;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
+        //super.onCreateView(inflater, group, bundle);
         View rootView = inflater.inflate(R.layout.fragment_family_member, container, false);
 
         return rootView;
